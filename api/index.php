3cc968dffc6f25bf4592c7e5c75721f99739b67a
Thomas Steinke <exyphnos@gmail.com>
2014-10-22 06:56:02 -0700

    Time for school

--- api/index.php
+++ api/index.php
@@ -1,28 +1,54 @@
 <?php
 include("db.php");
 
-include("quick.php");
+include("weebly_page.php");
 
 $request = array_slice(explode("/", $_SERVER[REQUEST_URI]), 2);
 $method = $_SERVER[REQUEST_METHOD];
 
-switch($request[0]) {
-  case "quick":
-    quickAndEasy($connect, $request, $method);
-    break;
-  default:
-    echo "Welcome to my secret hideout!";
-}
+handleRequest($connect, $request, $method);
 
-function refValues($arr){
-    if (strnatcmp(phpversion(),'5.3') >= 0) //Reference is required for PHP 5.3+
-    {
-        $refs = array();
-        foreach($arr as $key => $value)
-            $refs[$key] = &$arr[$key];
-        return $refs;
+function handleRequest($mysqli, $request, $method) {
+  var_dump($request);
+  var_dump($method);
+
+  die();
+
+  if($method == "POST") {
+    echo $_POST["method"];
+  }
+
+  //header('HTTP/1.0 404 Not Found');
+  //die();
+  //send404AndDie();
+
+  // Base stuff
+  if(count($request) == 1) {
+    switch($method) {
+      case "GET":
+        listApps(null);
+        break;
+      default:
+        echo "Not recognized, sorry bud";
     }
-    return $arr;
+  }
+
+  // Not base list - pass it on.
+  // Better have a hash!
+  else {
+    // Is it a hash?
+    if(strlen($request[1]) > 0) {
+      // Found app! Pass on the request
+      $app = new App($mysqli, $request[1], null);
+      $request = array_slice($request, 2);
+
+      // Pass on request!
+      $app->request($mysqli, $request, $method);
+    }
+    else {
+      echo("Error: App Hash not found");
+    }
+  }
 }
 
 function send404AndDie() {
